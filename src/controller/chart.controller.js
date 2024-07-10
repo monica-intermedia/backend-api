70ac2592baa90b66a277d2c4f326fc833ea8875f
Chandra Halim <chan.halim98@gmail.com>
2024-07-09 22:21:53 -0700

    f

--- src/controller/chart.controller.js
+++ src/controller/chart.controller.js
@@ -1,3 +1,6 @@
+const { Op, fn, col } = require("sequelize");
+const moment = require("moment");
+
 const PegawaiModels = require("../models/pegawai.models");
 const SupllierModels = require("../models/supplier.models");
 const BarangModels = require("../models/barang.models");
@@ -68,9 +71,70 @@ const getDataTransaksi = async (req, res) => {
   }
 };
 
+const getPendapatanMingguan = async (req, res) => {
+  try {
+    const today = moment().startOf("day"); // Get the start of today
+    const oneWeekAgo = moment().subtract(6, "days").startOf("day"); // Get the start of 7 days ago
+
+    const getTransaksiWeeks = await TransaksiModels.findAll({
+      where: {
+        status: "success",
+        createdAt: {
+          [Op.between]: [oneWeekAgo.toDate(), today.toDate()],
+        },
+      },
+      attributes: [
+        [fn("DATE", col("createdAt")), "date"],
+        [fn("SUM", col("gross_amount")), "gross_amount"],
+      ],
+      group: [fn("DATE", col("createdAt"))],
+      order: [[fn("DATE", col("createdAt")), "ASC"]],
+    });
+
+    if (getTransaksiWeeks.length === 0) {
+      handle400(req, res, "fail get data");
+    } else {
+      handle200(req, res, getTransaksiWeeks, "success get data");
+    }
+  } catch (error) {
+    console.error(error);
+    handle500(req, res, "internal server error");
+  }
+};
+
+const getTransaksiHariIni = async (req, res) => {
+  try {
+    const today = moment().startOf("day").toDate(); // Get the start of today
+    const endOfToday = moment().endOf("day").toDate(); // Get the end of today
+
+    const getTransaksiToday = await TransaksiModels.findAll({
+      where: {
+        status: "success",
+        createdAt: {
+          [Op.between]: [today, endOfToday],
+        },
+      },
+      attributes: [[fn("DATE", col("createdAt")), "date"], "namaKoran"],
+      group: [fn("DATE", col("createdAt"))],
+      order: [[fn("DATE", col("createdAt")), "ASC"]],
+    });
+
+    if (getTransaksiToday.length === 0) {
+      handle400(req, res, "fail get data");
+    } else {
+      handle200(req, res, getTransaksiToday, "success get data");
+    }
+  } catch (error) {
+    console.error(error);
+    handle500(req, res, "internal server error");
+  }
+};
+
 module.exports = {
   getChartDataPegawai,
   getChartDataSupplier,
   getChartDataBarang,
   getDataTransaksi,
+  getPendapatanMingguan,
+  getTransaksiHariIni,
 };
--- src/routes/chart.routes.js
+++ src/routes/chart.routes.js
@@ -6,6 +6,8 @@ const {
   getChartDataSupplier,
   getDataTransaksi,
   getChartDataBarang,
+  getPendapatanMingguan,
+  getTransaksiHariIni,
 } = require("../controller/chart.controller");
 
 router.get("/chart/pembelian", getKasKeluar);
@@ -13,5 +15,7 @@ router.get("/chart/pegawai", getChartDataPegawai);
 router.get("/chart/supplier", getChartDataSupplier);
 router.get("/chart/transaksi", getDataTransaksi);
 router.get("/chart/barang", getChartDataBarang);
+router.get("/chart/pendapatanmingguan", getPendapatanMingguan);
+router.get("/chart/transaksihariini", getTransaksiHariIni);
 
 module.exports = router;
